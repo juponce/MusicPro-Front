cfc3121aa018bd936dd8446875dae256eb0069ed
juponce <ju.ponce@duocuc.cl>
2023-04-19 12:13:28 -0400

    tarea

--- MusicPro/MusicPro/urls.py
+++ MusicPro/MusicPro/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
+    # Core Paths
+    path('', include('core.urls')),
+    # Admin Paths
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
