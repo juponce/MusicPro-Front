13c2ae613450222dd77356873eeb77e144119863
juponce <ju.ponce@duocuc.cl>
2023-05-01 19:35:10 -0400

    registro por api

--- MusicPro/registration/urls.py
+++ MusicPro/registration/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', signup_view, name='signup'),
     path('signin/', login_view, name='signin')
 ]
