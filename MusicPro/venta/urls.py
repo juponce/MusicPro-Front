383f54c91f92306fbeb810c4ae2b2ccb629f2863
ju.ponce <ju.ponce@duocuc.cl>
2023-05-03 12:51:27 -0400

    Avance productos

--- MusicPro/venta/urls.py
+++ MusicPro/venta/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', ProductsPageView.as_view(), name="productos")
+    path('', products_page_view, name="catalogo"),
+    path('carrito/', carrito, name='carrito'),
 ]
--- MusicPro/venta/views.py
+++ MusicPro/venta/views.py
@@ -4,8 +4,29 @@ from .services import get_product
 
 # Create your views here.
 
-class ProductsPageView(TemplateView):
+def modificar_productos(request):
     template_name = "venta/productos.html"
+    title = 'Productos'
+    product = get_product()
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title': 'productos', 'product' : get_product()})
+    return render(request, template_name, {'title': title, 'product': product})
+
+
+def products_page_view(request):
+    template_name = "venta/catalogo.html"
+    title = 'Catálogo'
+    product = get_product()
+
+    return render(request, template_name, {'title': title, 'product': product})
+
+def carrito(request):
+    if request.user.is_authenticated:
+        user = request.user
+        carrito, created = Carrito.objects.get_or_create(usuario=user)
+        items = carrito.itemcarrito_set.all()
+    else:
+        items = []
+        carrito = {'get_total_cart': 0}
+
+    data = {'items': items, 'carrito': carrito, 'dona': dona}
+    return render(request, 'venta/carrito.html', data)
